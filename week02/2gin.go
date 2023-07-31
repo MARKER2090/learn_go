d7052c8f44bfb24e39330d19e381a06e905410e2
diyun <diyundediyun@126.com>
2023-07-31 19:59:45 +0800

    update

--- week02/2gin.go
+++ week02/2gin.go
@@ -4,6 +4,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +15,7 @@ func main() {
 	router.GET("/hello", func(c *gin.Context) {
 		//c.String(http.StatusOK, "你好！这是gin web框架")
 		c.String(300, "你好！这是gin web框架")
+		fmt.Println(c.Get("hello"))
 	})
 
 	router.Run(":8082") //运行框架
