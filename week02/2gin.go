f7ce45041da8e133c64f5fce1fbe2fa29a2c316d
diyun <diyundediyun@126.com>
2023-07-31 23:01:19 +0800

    update

--- week02/2gin.go
+++ week02/2gin.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,10 +13,29 @@ import (
 func main() {
 	router := gin.Default() //新建一个engin
 
+	//静态路由
 	router.GET("/hello", func(c *gin.Context) {
 		//c.String(http.StatusOK, "你好！这是gin web框架")
 		c.String(300, "你好！这是gin web框架")
 		fmt.Println(c.Get("hello"))
+	})
+
+	//参数路由
+	router.GET("/users/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "这是你传过来的名字%s", name)
+	})
+
+	//通配符路由
+	router.GET("/views/*.html", func(c *gin.Context) {
+		path := c.Param(".html")
+		c.String(http.StatusOK, "这是你传过来的名字%s", path)
+	})
+
+	//查询参数
+	router.GET("/order", func(c *gin.Context) {
+		id := c.Query("id")
+		c.String(http.StatusOK, "这是你传过来的名字%s", id)
 	})
 
 	router.Run(":8082") //运行框架
